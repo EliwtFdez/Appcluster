2b92c45a9af9480578248cd6568ebdb2c517217e
EliwtFdez <shadowkadizz@gmail.com>
2025-02-06 00:38:04 -0600

    CommonModule resuelto, PagoFactura Ux Terminada la base

--- src/app/desktop-home/comunicacion/comunicacion.component.ts
+++ src/app/desktop-home/comunicacion/comunicacion.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-comunicacion',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './comunicacion.component.html',
   styleUrl: './comunicacion.component.scss'
 })
 export class ComunicacionComponent {
+casas = [
+    {
+      nombre: "Casa 1",
+      mantenimiento: false,
+      pagos: [
+        { descripcion: "Luz", monto: 50, fecha: new Date(), pagado: false },
+        { descripcion: "Agua", monto: 30, fecha: new Date(), pagado: true }
+      ]
+    }
+  ];
 
+  completarMantenimiento(casa: any) {
+    casa.mantenimiento = true;
+  }
+
+  marcarComoPagado(pago: any) {
+    pago.pagado = true;
+  }
+
+  trackById(index: number, item: any) {
+    return item.nombre;
+  }
 }
