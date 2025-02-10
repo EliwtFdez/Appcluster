0130da0feb6e26ab21760bfcd72a080b28408e1f
EliwtFdez <shadowkadizz@gmail.com>
2025-02-10 14:50:14 -0600

    Ux con animacion

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { Router, RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-register',
@@ -15,15 +18,16 @@ import { Router, RouterModule } from '@angular/router';
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatSelectModule,
     CommonModule,
     RouterModule,
     MatProgressSpinnerModule,
     FormsModule,
-    ],
-    templateUrl: './register.component.html',
-    styleUrls: ['./register.component.scss']
+    MatSnackBarModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
 })
-
 export class RegisterComponent {
   email: string = '';
   username: string = '';
@@ -31,8 +35,11 @@ export class RegisterComponent {
   confirmPassword: string = '';
   isLoading: boolean = false;
   error: string = '';
+  houseNumbers: number[] = Array.from({ length: 60 }, (_, i) => i + 1);
+  houseNumber: any;
 
-  constructor(private router: Router, private snackBar: MatSnackBar) {}
+constructor(private router: Router, private snackBar: MatSnackBar) {}
+
 
   isEmailValid(email: string): boolean {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -66,15 +73,10 @@ export class RegisterComponent {
       this.isLoading = true;
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      console.log('Email:', this.email);
-      console.log('Username:', this.username);
-      console.log('Password:', this.password);
-
       this.snackBar.open('Registration successful!', 'Close', { duration: 3000 });
       this.router.navigate(['/login']);
     } catch (err) {
       this.error = 'An error occurred during registration';
-      console.error(err);
     } finally {
       this.isLoading = false;
     }
